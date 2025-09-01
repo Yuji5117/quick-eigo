ed6527485029b127ed201bcccbeccea1e2c98894
Yuji Nakamura <ny5117@icloud.com>
2025-09-01 15:14:32 +1200

    テキストのスタイルを修正

--- src/features/practice/components/QuestionText.tsx
+++ src/features/practice/components/QuestionText.tsx
@@ -4,8 +4,8 @@ type QuestionTextProps = {
 
 export const QuestionText: React.FC<QuestionTextProps> = ({ text }) => {
   return (
-    <div className="rounded-lg bg-gray-100 p-3">
-      <p className="text-center font-semibold">{text}</p>
+    <div className="rounded-lg bg-gray-100 px-3 py-6">
+      <p className="text-center text-lg font-semibold">{text}</p>
     </div>
   )
 }
